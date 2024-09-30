652f5c27965ca0f971c8a169bfce5c6dbba03a71
Basil Abushihab <basilabushihab22@gmail.com>
2024-09-30 06:24:10 +0300

    changes

--- nextJS-Task/next-task/src/app/Products/page.jsx
+++ nextJS-Task/next-task/src/app/Products/page.jsx
@@ -1,23 +1,28 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
-import { Card, CardBody } from "@material-tailwind/react";
+import { useRouter } from "next/navigation";
 
-export default function Products() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("https://fakestoreapi.com/products");
-      setProducts(response.data);
-    };
-    fetchData();
-  });
+export default async function Products() {
+  const data = await axios.get("https://fakestoreapi.com/products");
+  const router = useRouter();
   return (
     <div>
-      <Card>
-        <CardBody>Hello</CardBody>
-      </Card>
+      {data.data.map((data, index) => (
+        <div key={index} className="cursor-pointer" onClick={()=>{router.push()}}>
+          <div className="flex flex-col ">
+            <Image
+              width={300}
+              height={300}
+              className=""
+              src={data.image}
+              alt=""
+            />
+            <h4>Title:{data.title}</h4>
+            <h4>Price:{data.price}</h4>
+            <h4>ID:{data.id}</h4>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
